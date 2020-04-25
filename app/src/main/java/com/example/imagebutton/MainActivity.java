6df0de0bbdcf0c8742d0831be715b6a15e31c5ca
Rohitohlyan66 <ohlyanrohit66@gmail.com>
2020-04-25 13:32:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/imagebutton/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.imagebutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton imageButton=findViewById(R.id.image_button);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button is Pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
